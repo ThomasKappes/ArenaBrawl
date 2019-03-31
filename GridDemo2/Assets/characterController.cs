587ccce3246755b2dfd5e286136a4815049adc75
Maximilian Gatterer <maxg319@gmail.com>
2019-03-31 15:30:02 +0200

    character animation
    added script for walking animation incl controller

---
+++ GridDemo2/Assets/characterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("up")) {
+            m_Animator.SetBool("isWalking", true);
+        } else {
+            m_Animator.SetBool("isWalking", false);
+        }
+    }
+}
