73feb5add7d6005646f44956820542a724948d15
Maximilian Gatterer <maxg319@gmail.com>
2019-03-31 15:46:00 +0200

    made running possible
    added running to controller

--- GridDemo2/Assets/characterController.cs
+++ GridDemo2/Assets/characterController.cs
@@ -14,10 +14,15 @@ public class characterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("up")) {
+        if(Input.GetKey(KeyCode.W)) {
             m_Animator.SetBool("isWalking", true);
         } else {
             m_Animator.SetBool("isWalking", false);
+        }
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            m_Animator.SetBool("isRunning", true);
+        } else {
+            m_Animator.SetBool("isRunning", false);
         }
     }
 }
