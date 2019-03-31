521461cece1a4d638176ed929c2f717d996a0bdd
Maximilian Gatterer <maxg319@gmail.com>
2019-03-31 19:19:55 +0200

    added rotatability

--- GridDemo2/Assets/characterController.cs
+++ GridDemo2/Assets/characterController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class characterController : MonoBehaviour
 {
     Animator m_Animator;
     bool walking = false;
     bool running = false;
+    public float turnSpeed;
+    public Camera cam;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,15 @@ public class characterController : MonoBehaviour
         updateBooleans();
         m_Animator.SetBool("isWalking", walking);
         m_Animator.SetBool("isRunning", running);
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPos = cam.WorldToScreenPoint(transform.position);
+        float bog = (float) Math.Atan((mousePosition.y-screenPos.y)/(mousePosition.x-screenPos.x));
+        float degree = ((bog * 180) / (float) Math.PI);
+        float toRotate = (90 - degree);
+
+        transform.rotation = Quaternion.AngleAxis((mousePosition.x-screenPos.x) >= 0 ? toRotate : 180 + toRotate, Vector3.up);
+
+
     }
 
     void updateBooleans()
