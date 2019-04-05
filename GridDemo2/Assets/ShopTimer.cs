3b9a0c0cdc6348e3c17c326665193439b820eeda
Thomas Kappes <thomas.kappes.173@gmail.com>
2019-04-05 18:46:35 +0200

    updates Basic ShopLayout

---
+++ GridDemo2/Assets/ShopTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopTimer : MonoBehaviour
+{
+    public int duration = 60;
+    public TextMeshProUGUI text;
+    private int timeRemaining;
+
+   
+    void Start()
+    {
+        text.text = duration + " Secounds remaining";
+        timeRemaining = duration;
+        _tick();
+    }
+
+    private void _tick()
+    {
+        timeRemaining--;
+        if (timeRemaining > 0)
+        {
+            text.text = timeRemaining + " Secounds remaining";
+            Debug.Log("Time remaining: " + timeRemaining);
+            Invoke("_tick", 1f);
+        }
+        else
+        {
+            Debug.Log("Timer finished.");
+        }
+    }
+}
