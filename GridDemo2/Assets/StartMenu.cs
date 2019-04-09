708feb0ba20809130748f9472fe1a14a41921109
Thomas Kappes <thomas.kappes.173@gmail.com>
2019-04-09 18:34:31 +0200

    Adds Basic Startup

---
+++ GridDemo2/Assets/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public Animator animator;
+
+    public void PlayFadeOut()
+    {
+
+        animator.SetTrigger("FadeOut");
+
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            PlayFadeOut();
+        }
+    }
+
+    private void changeToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
